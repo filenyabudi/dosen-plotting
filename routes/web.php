2a375e65eb4037a3aee16ba5293b02510782d4a5
Budi Ramdani <filenyabudi@gmail.com>
2025-01-07 10:16:35 +0700

    perbaikan route

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::resource('dosens', App\Http\Controllers\DosenController::class)->except('destroy');
-
-Route::resource('matakuliahs', App\Http\Controllers\MatakuliahController::class)->except('destroy');
-
-Route::resource('plottings', App\Http\Controllers\PlottingController::class)->except('destroy');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
